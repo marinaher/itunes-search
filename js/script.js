74eb37bce9b6a1e16aec7f1b08cd35166062eceb
marinaher <b-her@live.com>
2016-09-29 11:24:31 -0500

    added url functions and showonpage function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,50 @@
-/*var url = 'http://itunes.apple.com/search?' + params;
-var html = '<script src="' + url + '"><\/script>';
-$('head').append(html);*/
+$(document).ready(function(){
+$("#searchButton").click(function() {
 
-var userInput = $("input").val;
-var url = 'http://itunes.apple.com/search?' + userInput;
+    var userInput = $("#searchField").val();
 
-/*var request = new XMLHttpRequest();
-request.open("GET", "jQuery.js", false);
-request.send();
-console.log(request);*/
+    $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        url: "https://itunes.apple.com/search?term=" + userInput + "&limit=25",
+        success: function(data) {
+            // Another function to loop and display
+            console.log(data);
+            $("#searchField").val("");
+            showOnPage(data, userInput);
+        }
+    });
+});
+
+
+var showOnPage = function(data, userInput) {
+    var dataArray = data.results;
+    $("#resultsLabel").fadeIn(200);
+    $("#resultsCount").text("");
+    $("#resultsCount").text(data.resultCount + " results for " + userInput);
+    dataArray.forEach(function(arrayElement) {
+        console.log(/*arrayElement.collectionName*/);
+    });
+    /*for (var i = 0; i <= data.resultCount; i++) {
+    	console.log(dataArray[i].collectionName);
+    }*/
+}
+
+/*var handleSearchResults = function(data){
+	var results = data.results;
+	$('#displaySearchResults').fadeIn(200);
+	$('#displaySearchResults').text("");
+	for(var i = 0; i < data.resultsCount; i++){
+		var item = results[i];
+		var obj = {
+			source = 0,
+			artworkUrl100 = item.artworkUrl100,
+			collectionName = item.collectionName,
+			primaryGenreName = item.primaryGenreName,
+			trackCount = item.trackCount,
+			trackName = item.trackName,
+			currency = item.currency
+		}	
+	}
+}*/
+});
