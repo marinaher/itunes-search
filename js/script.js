1ebe04bbc740730c801be1dd1b21abf50159fe19
marinaher <b-her@live.com>
2016-10-02 22:17:15 -0500

    added 'show more' pagination

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
+    var myData;
+    var myUserInput;
+    var startIndex = 0;
+    var maxNumberPerPage = 10;
+
     //Click Event
     $("#searchButton").click(function() {
         search();
@@ -12,6 +17,10 @@ $(document).ready(function() {
         }
     });
 
+    $('#showMore').click(function() {
+        showOnPage();
+    });
+
     // Search
     var search = function() {
         var userInput = $("#searchField").val();
@@ -21,55 +30,69 @@ $(document).ready(function() {
             url: "https://itunes.apple.com/search?term=" + userInput + "&limit=50",
             success: function(data) {
                 $("#searchField").val("");
-                // console.log(data);
-                showOnPage(data, userInput);
+                myData = data;
+                // console.log(myData);
+                myUserInput = userInput;
+                showOnPage(userInput);
             }
         });
     }
 
     // Render Elements
-    var showOnPage = function(data, userInput) {
-        var songInfoItems = createListItems(data);
+    var showOnPage = function() {
+        var songInfoItems = createListItems();
 
         // Show/Hide DOM
         $("#resultsLabel").fadeIn(200);
         $("#resultsCount").text("");
-        $("#resultsCount").text(data.resultCount + " results for \"" + userInput + "\".");
+        $("#resultsCount").text(myData.resultCount + " results for \"" + myUserInput + "\".");
         $("#searchResultsList").empty();
         $("#searchResultsList").fadeIn(200);
 
+
+        maxNumberPerPage += startIndex;
+
         // Render
         $('#searchResultsList').html(songInfoItems);
+
     }
 
     // Loop and Create elements
-    var createListItems = function(data) {
-        var dataItems = data.results;
+    var createListItems = function() {
+        var dataItems = myData.results;
         var songInfoItems = '';
 
-        for (var i = 0; i < dataItems.length; i++) {
-            var item = dataItems[i];
-            var songInfo = {
-                source: 0,
-                artwork_url: item.artworkUrl100,
-                track_name: item.trackCensoredName,
-                artist_name: item.artistName,
-                collection_name: item.collectionCensoredName,
-                genre: item.primaryGenreName,
-                release_date: item.releaseDate,
-                preview_url: item.previewUrl
-            };
+        if (maxNumberPerPage > dataItems.length) {
+            maxNumberPerPage = dataItems.length;
+        }
 
-            dataItems[i] = songInfo;
-            songInfoItems += '<li class="songListItem col-sm-6 col-md-3 flex-item">';
-            songInfoItems += '<img class="artWork" src=' + songInfo.artwork_url.replace('100x100', '260x260') + '/>';
-            songInfoItems += '<p><video controls="" height="40" width="100%" name="media"><source src="' + songInfo.preview_url + '"type="audio/mp4"></video></p>';
-            songInfoItems += '<h4 class="songTrackName">' + songInfo.track_name + '</h4>';
-            songInfoItems += '<h5 class="artistname">' + songInfo.artist_name + '</h5>';
-            songInfoItems += '<p class="collenctionName">' + songInfo.collection_name + '</p>';
-            songInfoItems += '<p class="genre"><small>' + songInfo.genre + '</small></p>';
+        for (startIndex; startIndex < maxNumberPerPage; startIndex++) {
 
-            songInfoItems += "</li>";
+            if (typeof dataItems[startIndex] !== "undefined") {
+
+                var item = dataItems[startIndex];
+                var songInfo = {
+                    source: 0,
+                    artwork_url: item.artworkUrl100,
+                    track_name: item.trackCensoredName,
+                    artist_name: item.artistName,
+                    collection_name: item.collectionCensoredName,
+                    genre: item.primaryGenreName,
+                    release_date: item.releaseDate,
+                    preview_url: item.previewUrl
+                };
+
+                dataItems[startIndex] = songInfo;
+                songInfoItems += '<li class="songListItem col-sm-6 col-md-3 flex-item">';
+                songInfoItems += '<img class="artWork" src=' + songInfo.artwork_url.replace('100x100', '260x260') + '/>';
+                songInfoItems += '<p><video controls="" height="40" width="100%" name="media"><source src="' + songInfo.preview_url + '"type="audio/mp4"></video></p>';
+                songInfoItems += '<h4 class="songTrackName">' + songInfo.track_name + '</h4>';
+                songInfoItems += '<h5 class="artistname">' + songInfo.artist_name + '</h5>';
+                songInfoItems += '<p class="collenctionName">' + songInfo.collection_name + '</p>';
+                songInfoItems += '<p class="genre"><small>' + songInfo.genre + '</small></p>';
+
+                songInfoItems += "</li>";
+            }
         }
 
         return songInfoItems;
@@ -82,13 +105,6 @@ $(document).ready(function() {
         } else {
             $('.scrollToTop').fadeOut();
         }
-    });
-
-    //Pagination
-    $('#placeholder').easyPaginate({
-        paginateElement: 'li',
-        elementsPerPage: 16,
-        effect: 'default'
     });
 
     //Click event to scroll to top
